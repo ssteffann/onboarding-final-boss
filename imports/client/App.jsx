4163ec4d56466d5558f5e007f4cabee58a1c873e
claudiu <claudiurom2006@gmail.com>
2017-12-08 15:18:15 +0200

    updated footer / header / app.jsx

--- imports/client/App.jsx
+++ imports/client/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import Header from '/imports/client/layout/header/Header';
-import Footer from '/imports/client/layout/footer/Footer';
 
-export default ({main, routeProps}) => {
-    // main represents the component to render passed from the router
-    // route props represent the properties that it receives from the router
+export default class App extends React.Component {
+    constructor () {
+        super();
 
-    // where we do createElement, that's where your components will get rendered.
-    return (
-        <div id="app">
-            <Header/>
-            {React.createElement(main, routeProps)}
-            <Footer/>
-        </div>
-    )
+    }
+
+    render() {
+        const {main, routeProps} = this.props;
+
+        return [
+            React.createElement(main, {routeProps: routeProps, key: 'main'})
+        ]
+    }
 }
